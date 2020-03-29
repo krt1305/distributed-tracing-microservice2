eb51605f33a61bd42859e90f10dd56a708b04cfb
Rabia <krt1305@gmail.com>
2020-03-29 23:24:01 +0530

    first commit

---
+++ src/test/java/com/example/distributed/tracing/microservice2/Microservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributed.tracing.microservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
